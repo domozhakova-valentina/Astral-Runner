d0f52af1264ccac9e988223bcfcec3e36c2222d8
Ivan Gaideek <workgaideek37@gmail.com>
2023-12-24 16:15:16 +0300

    add player v1

--- level.py
+++ level.py
@@ -1,18 +1,23 @@
 import pygame
-from settings import tile_size
+from settings import tile_size, screen_width, screen_height
 from load import import_csv_map, import_folder_images
 from maps_odject import StaticTile, AnimatedDecor, Coin
+from player import Player
 
 
 class Level:
     def __init__(self, data_level, screen):
         # общая настройка
         self.display = screen
-        self.world_shift = -3  # сдвиг мира он нужен для того чтобы прокручевать карту(все объекты её)
+        self.world_shift = pygame.math.Vector2(0, 0)  # сдвиг мира он нужен для того чтобы прокручевать карту(все объекты её)
 
         # игрока
         player_map = import_csv_map(data_level['player'])
-        self.designations_players = self.create_tiles_group(player_map, 'player')
+        self.player = pygame.sprite.GroupSingle()
+        self.purpose = pygame.sprite.GroupSingle()
+        self.start = pygame.sprite.GroupSingle()
+        self.x = None
+        self.load_player(player_map)
 
         # настройка рельефа местности(плиток)
         terrain_map = import_csv_map(data_level['terrain'])
@@ -37,15 +42,96 @@ class Level:
                     if type in ('terrain', 'decorations'):
                         surfaces = import_folder_images('graphics/tirrein')
                         image = surfaces[int(col)]  # берём по индификатуру из списка изображение
-                        sprite = StaticTile(tile_size, x, y, image)
+                        if type == 'decorations':
+                            sprite = StaticTile(tile_size, x, y, image, croped=(0, 50))
+                        else:
+                            sprite = StaticTile(tile_size, x, y, image)
                     elif type == 'coins':
                         sprite = Coin(tile_size, x, y, 'graphics/coins')
-                    elif type == 'player':
-                        image = pygame.image.load(['graphics/character/start.png',
-                                                   'graphics/character/end.png'][int(col)]).convert_alpha()
-                        sprite = StaticTile(tile_size, x, y, image)
                     all_group.add(sprite)
         return all_group
+
+    def load_player(self, map):
+        '''Загрузка игрока, места возрождения, цели до которой он должен добраться.'''
+        for row_ind, row in enumerate(map):
+            for col_ind, col in enumerate(row):
+                if col != '-1':
+                    x = col_ind * tile_size
+                    y = row_ind * tile_size
+                    if col == '0':
+                        sprite = Player((x, y), self.display, 'graphics/character_animate/', speed=7)
+                        self.player.add(sprite)
+                        image = pygame.image.load('graphics/character/start.png').convert_alpha()
+                        sprite = StaticTile(tile_size, x, y, image)
+                        self.start.add(sprite)
+                    elif col == '1':
+                        image = pygame.image.load('graphics/character/end.png').convert_alpha()
+                        sprite = StaticTile(tile_size, x, y, image)
+                        self.purpose.add(sprite)
+
+    def horizontal_collisions(self):
+        '''Горизонтальные столкновения с картой.'''
+        sprite = self.player.sprite
+        group_sprites = self.terrain_sprites.sprites() + self.fg_decorations.sprites()
+        sprite.rect.x += sprite.direction.x * sprite.speed
+        for gs in group_sprites:
+            if pygame.sprite.collide_mask(sprite, gs):
+                if sprite.direction.x < 0:
+                    sprite.rect.left = gs.rect.right
+                    sprite.left = True
+                    self.x = sprite.rect.left
+                if sprite.direction.x > 0:
+                    sprite.rect.right = gs.rect.left
+                    sprite.right = True
+                    self.x = sprite.rect.right
+            if sprite.left and (sprite.rect.left < self.x or sprite.direction.x >= 0):
+                sprite.left = False
+            if sprite.right and (sprite.rect.right > self.x or sprite.direction.x <= 0):
+                sprite.right = False
+
+    def vertical_collisions(self):
+        '''Вертикальные столкновения с картой.'''
+        sprite = self.player.sprite
+        group_sprites = self.terrain_sprites.sprites() + self.fg_decorations.sprites()
+        sprite.gravitation()
+        for gs in group_sprites:
+            if gs.rect.colliderect(sprite.rect):
+                if sprite.direction.y < 0:
+                    sprite.rect.top = gs.rect.bottom
+                    sprite.direction.y = 0
+                    sprite.ceiling = True
+                elif sprite.direction.y > 0:
+                    sprite.rect.bottom = gs.rect.top
+                    sprite.direction.y = 0
+                    sprite.ground = True
+        if sprite.ground and sprite.direction.y != 0:
+            sprite.ground = False
+        if sprite.ceiling and sprite.direction.y > 0:
+            sprite.ceiling = False
+
+    def moving_map(self):
+        '''Прокручивание экрана.'''
+        sprite = self.player.sprite
+        mid_x, dir_x, mid_y, dir_y = sprite.rect.centerx, sprite.direction.x, sprite.rect.centery, sprite.direction.y
+        if mid_x < screen_width / 3 and dir_x < 0:
+            self.world_shift.x = sprite.CONST_SPEED
+            sprite.speed = 0
+        elif mid_x > screen_width - (screen_width / 3) and dir_x > 0:
+            self.world_shift.x = -sprite.CONST_SPEED
+            sprite.speed = 0
+        else:
+            self.world_shift.x = 0
+            sprite.speed = sprite.CONST_SPEED
+        if mid_y > screen_height - (screen_height / 4) and dir_y < 0:
+            self.world_shift.y = -4
+        elif mid_y < screen_height / 4 and dir_y > 0:
+            self.world_shift.y = 2
+        elif (screen_height / 2 - screen_height / 10) < mid_y < (screen_height / 2 + screen_height / 10):
+            self.world_shift.y = 0
+        elif (screen_height / 2 - screen_height / 10) > mid_y:
+            self.world_shift.y = 3
+        else:
+            self.world_shift.y = -6
 
     def run(self):
         '''Запуск уровня!'''
@@ -59,6 +145,16 @@ class Level:
         self.coins.update(self.world_shift)
         self.coins.draw(self.display)
 
+        # рисование игрока
+        self.player.update()
+        self.vertical_collisions()
+        self.horizontal_collisions()
+        self.moving_map()
+        self.player.draw(self.display)
+
         # рисование возрождения и цели - конца
-        self.designations_players.update(self.world_shift)
-        self.designations_players.draw(self.display)
+        self.start.update(self.world_shift)
+        self.start.draw(self.display)
+
+        self.purpose.update(self.world_shift)
+        self.purpose.draw(self.display)
---
+++ player.py
@@ -0,0 +1,85 @@
+import pygame
+from load import import_folder_images
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, position, screen, character_path, speed=5):
+        super().__init__()
+        self.import_character_animations(character_path)
+        # про анимацию
+        self.index_cadre = 0
+        self.an_speed = 0.1  # коэффициент перемотки кадров
+        self.image = self.animations['idle'][self.index_cadre]
+        self.action = 'idle'
+        # физические величины
+        self.direction = pygame.math.Vector2(0, 0)
+        self.CONST_SPEED = speed
+        self.speed = speed
+        self.gravity = 0.6
+        self.jump_coef = -16
+        self.rect = self.image.get_rect(topleft=position)  # установка на позицию
+        self.mask = pygame.mask.from_surface(self.image)
+        self.face_right = True
+        self.ground = False  # стоит на земле
+        self.ceiling = False  # касается потолка
+        self.left, self.right = False, False  # касается чего-то справа или слева соответственно
+
+    def import_character_animations(self, path):
+        self.animations = {'run': None, 'jump': None, 'idle': None}  # словарь с изображениями каждого вида анимации
+        for animate in self.animations.keys():
+            self.animations[animate] = import_folder_images(path + animate)
+
+    def animation(self):
+        '''Выборка нужного кадра героя.'''
+        cadres = self.animations[self.action]
+        self.index_cadre += self.an_speed
+        if self.index_cadre > len(cadres):
+            self.index_cadre = 0
+        cadre = cadres[int(self.index_cadre)]
+        self.image = cadre if self.face_right else pygame.transform.flip(cadre, True, False)
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect(topleft=self.rect.topleft)
+        if self.ground and self.right:
+            self.rect = self.image.get_rect(bottomright=self.rect.bottomright)
+        elif self.ground and self.left:
+            self.rect = self.image.get_rect(bottomleft=self.rect.bottomleft)
+        elif self.ceiling and self.right:
+            self.rect = self.image.get_rect(topright=self.rect.topright)
+        elif self.ceiling and self.left:
+            self.rect = self.image.get_rect(topleft=self.rect.topleft)
+        elif self.ground:
+            self.rect = self.image.get_rect(midbottom=self.rect.midbottom)
+        elif self.ceiling:
+            self.rect = self.image.get_rect(midtop=self.rect.midtop)
+
+    def tracking_action(self):
+        if self.direction.y < 0:
+            self.action = 'jump'
+        elif self.direction.x == 0:
+            self.action = 'idle'
+        elif self.direction.x != 0:
+            self.action = 'run'
+
+    def user_input(self):
+        '''Изменение вектора передвижения игрока в соответствии с нажатыми клавишами'''
+        input_keys = pygame.key.get_pressed()
+        if input_keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+            self.face_right = True
+        elif input_keys[pygame.K_LEFT]:
+            self.direction.x = -1
+            self.face_right = False
+        else:
+            self.direction.x = 0
+        if input_keys[pygame.K_SPACE] and self.ground:
+            self.direction.y = self.jump_coef
+
+    def gravitation(self):
+        '''Изменение положения игрока под действием силы тяжести'''
+        self.direction.y += self.gravity
+        self.rect.y += self.direction.y
+
+    def update(self):
+        self.user_input()
+        self.animation()
+        self.tracking_action()
