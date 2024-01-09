bc03a30dbb1b5264ed6a814f83951354921cf75f
Valentina <domozhakova.valya@gmail.com>
2024-01-09 22:49:58 +0300

    Поменяла структуру меню

--- main.py
+++ main.py
@@ -9,48 +9,58 @@ from sounds import sounds_list, music_list, background_music, all_sounds
 from levels_map import Levels_Map, levels, button_actions
 
 pygame.init()
-pygame.mixer.set_num_channels(20) # устанавливаем больше звуковых каналов
-for elem in sounds_list: # добавление всех звуков
+pygame.mixer.set_num_channels(20)  # устанавливаем больше звуковых каналов
+for elem in sounds_list:  # добавление всех звуков
     all_sounds.add_sound(elem)
 
-for elem in music_list: # добавление всей музыки
+for elem in music_list:  # добавление всей музыки
     background_music.add_music(elem)
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('AstralRunner')
 scene = menu = Menu(items)
 menu.start_music()
-# game_over = Game_over(buttons)
-# options = Options(options_items)
-# level_0 = Level(coins_data.pickle, screen)
-# level_1 = Level(data_level_1.pickle, screen)
-# level_2 = Level(data_level_2.pickle, screen)
-# level_3 = Level(data_level_3.pickle, screen)
-# levels_map = Levels_Map(levels)
+game_over = Game_over(buttons)
+options = Options(options_items)
+levels_map = Levels_Map(levels, button_actions)
+level = Level(level_0, screen)
 clock = pygame.time.Clock()
 
-game_states = {
-    'running_levels_map': Levels_Map(levels, button_actions),
-    'running_rules': Menu(items),
-    'running_options': Options(options_items),
-    'running_menu': Menu(items),
-    'running_level_1': Level(level_0, screen),
-    'running_level_2': Level(level_1, screen),
-    'running_level_3': Level(level_2, screen),
-    'running_level_4': Level(level_3, screen),
-    'running_level_5': Level(level_0, screen),
-    'game_over': Game_over(buttons)
-}
-
 while True:
+    # if level.end_flag:
+    #     scene = Game_over(buttons)
+    #     level.end_flag = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         change = scene.update(event)
         if change is not None:
+            game_state = change()
             scene.stop_music()
-            scene = game_states[change()]
+            if game_state == 'running_menu':
+                scene = menu
+            if game_state == 'running_level_1':
+                level = Level(level_0, screen)
+                scene = level
+            if game_state == 'running_level_2':
+                level = Level(level_1, screen)
+                scene = level
+            if game_state == 'running_level_3':
+                level = Level(level_2, screen)
+                scene = level
+            if game_state == 'running_level_4':
+                level = Level(level_3, screen)
+                scene = level
+            if game_state == 'running_level_5':
+                level = Level(level_0, screen)
+                scene = level
+            if game_state == 'running_options':
+                scene = options
+            if game_state == 'running_rules':
+                pass
+            if game_state == 'running_levels_map':
+                scene = levels_map
             scene.start_music()
     scene.run(event, screen)
     pygame.display.flip()
