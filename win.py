7df63476795bb933a1b1db236610ba89e02a0b8e
Dasha <Dashaaf2006@gmail.com>
2024-01-10 00:29:06 +0300

    win

--- win.py
+++ win.py
@@ -3,14 +3,14 @@ from menu import Menu
 from settings import *
 
 buttons1 = [
-    Button("Меню", action=new_game_action, width=menu_item_width * 1.3, height=menu_item_height * 1.3,
+    Button("Меню", action=menu_action, width=menu_item_width * 1.3, height=menu_item_height * 1.3,
            color=(255, 255, 255),
            font=font, font_size=font_size, x=120, y=400),
-    Button("Следующий уровень", action=menu_action, width=menu_item_width * 1.3, height=menu_item_height * 1.3, color=(255, 255, 255),
+    Button("Следующий уровень", action=new_game_action, width=menu_item_width * 1.3, height=menu_item_height * 1.3, color=(255, 255, 255),
            font=font, font_size=font_size, x=750, y=400),
     Button("ВЫ ПОБЕДИЛИ!", action=None, width=game_over_width * 1.25, height=game_over_height * 1.25, color=(255, 255, 255),
            font=font, font_size=font_size * 2, x=290, y=100),
-    Button(f"Количество монет: 10", action=menu_action, width=menu_item_width, height=menu_item_height, color=(255, 255, 255),
+    Button(f"Количество монет: ", action=None, width=menu_item_width, height=menu_item_height, color=(255, 255, 255),
            font=font, font_size=font_size // 2, x=850, y=680)
 ]
 
