eddda6cc582072c4be3ac78128da7f98667e2d3d
Dasha <Dashaaf2006@gmail.com>
2024-01-10 00:29:34 +0300

    win

--- win.py
+++ win.py
@@ -6,9 +6,11 @@ buttons1 = [
     Button("Меню", action=menu_action, width=menu_item_width * 1.3, height=menu_item_height * 1.3,
            color=(255, 255, 255),
            font=font, font_size=font_size, x=120, y=400),
-    Button("Следующий уровень", action=new_game_action, width=menu_item_width * 1.3, height=menu_item_height * 1.3, color=(255, 255, 255),
+    Button("Следующий уровень", action=new_game_action, width=menu_item_width * 1.3, height=menu_item_height * 1.3,
+           color=(255, 255, 255),
            font=font, font_size=font_size, x=750, y=400),
-    Button("ВЫ ПОБЕДИЛИ!", action=None, width=game_over_width * 1.25, height=game_over_height * 1.25, color=(255, 255, 255),
+    Button("ВЫ ПОБЕДИЛИ!", action=None, width=game_over_width * 1.25, height=game_over_height * 1.25,
+           color=(255, 255, 255),
            font=font, font_size=font_size * 2, x=290, y=100),
     Button(f"Количество монет: ", action=None, width=menu_item_width, height=menu_item_height, color=(255, 255, 255),
            font=font, font_size=font_size // 2, x=850, y=680)
